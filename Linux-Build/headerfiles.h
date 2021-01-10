70b8d2a92f5c7482b9c643d501a88d0f2b5ff6be
Jensen Leo <jensen2944@gmail.com>
2021-01-10 14:56:21 +0800

    New features added for Meter Reader

--- Linux-Build/headerfiles.h
+++ Linux-Build/headerfiles.h
@@ -1,180 +1,173 @@
 #pragma once
-#include <iostream>
-#include <fstream>
 #include <string.h>
-//#include <direct.h>
-#include <iomanip>
+
+#include <fstream>
+#include <iostream>
 #include <ctime>
-//#include <io.h>
+#include <iomanip>
 #include <fcntl.h>
-//#include <Windows.h>
 #include <unistd.h>
 using namespace std;
 
-char FLOC_FILEFOLDER[70] = { "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/"};
-string FLOC_USERBASICINFO = "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/userbasicinfo.dat";
-string FLOC_EDITUSERINFO_TEMP = "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/temp_userbasicinfo.dat";
-string FLOC_BILLDETAIL = "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/bill_detail.dat";
-string FLOC_MRINFO = "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/mrinfo.dat";
-string FLOC_EDITMRINFO_TEMP = "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/temp_mrinfo.dat";
-string FLOC_RATE = "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/rate.dat";
-
+// File locations ====================================================
+char FLOC_FILEFOLDER[70] = {
+    "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/"};
+string FLOC_USERBASICINFO =
+    "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/"
+    "userbasicinfo.dat";
+string FLOC_EDITUSERINFO_TEMP =
+    "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/"
+    "temp_userbasicinfo.dat";
+string FLOC_BILLDETAIL =
+    "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/"
+    "bill_detail.dat";
+string FLOC_MRINFO =
+    "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/mrinfo.dat";
+string FLOC_EDITMRINFO_TEMP =
+    "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/"
+    "temp_mrinfo.dat";
+string FLOC_RATE =
+    "/home/jensen/Documents/Code/ElectricityBillingSystem/testdata/rate.dat";
+// Global Veriables =========================================================
 int totalUser = 0;
 const int N = 6;
-
-typedef struct fLoc_sysSet
-{
-	string userBasicInfo;
-	string editUserBasicInfo_tmp;
-}fLoc_sysSet;
-typedef struct address
-{
-	char city[32];
-	char district[32]; // Town
-	char street[32]; // County
-	char estate[32]; // Village
-	char unit[32]; // Unit
-	int level;
-	int room;
-}add;
-typedef struct date
-{
-	int sec;
-	int min;
-	int h;
-	int d;
-	int m;
-	int y;
-}date;
-typedef struct deviceinfo
-{
-	int model;
-	int serialNo;
-	int perchaseTime;
-}device;
-typedef struct userinfo
-{
-	double balance;
-	bool powercut;
-	// Read
-	date last_read;
-	bool read_now;
-	int last_month_usage;
-	//basic
-	int No;
-	char name[32];
-	char id[19];
-	int type;
-	add address;
-	char password[32];
-	double voltage;
-}userinfo;
-typedef struct userbill
-{
-	// General
-	int caseNo;
-	userinfo user_record;
-	int rateNo;
-	// Usage
-	int last_month_usage;
-	int current_usage;
-	// Read status
-	bool read;
-	date read_date;
-	// Pay Status
-	int pay;
-	bool payment;
-	date payment_date;
-}userbill;
-typedef struct MRdef
-{
-	int No;
-	char name[32];
-	float progress;
-}MRdef;
-typedef struct rateRecord
-{
-	date createTime;
-	double ent[N][N];
-	double urban[N][N];
-	double rural[N][N];
-}rateRecord;
-typedef struct estateUserInfoNode{
-	userinfo info;
-	struct estateUserInfoNode* next;
-}estateUserInfoNode;
-
-rateRecord getCurrentRate()
-{
-	rateRecord current;
-	fstream file;
-	file.open(FLOC_RATE, ios::binary | ios::in);
-	float size = sizeof(rateRecord);
-	file.seekg(-size, ios::end);
-	file.read((char*)&current, sizeof(rateRecord));
-	file.close();
-	return current;
+// Structures =========================================================
+typedef struct fLoc_sysSet {
+  string userBasicInfo;
+  string editUserBasicInfo_tmp;
+} fLoc_sysSet;
+typedef struct address {
+  char city[32];
+  char district[32];  // Town
+  char street[32];    // County
+  char estate[32];    // Village
+  char unit[32];      // Unit
+  int level;
+  int room;
+} add;
+typedef struct date {
+  int sec;
+  int min;
+  int h;
+  int d;
+  int m;
+  int y;
+} date;
+typedef struct deviceinfo {
+  int model;
+  int serialNo;
+  int perchaseTime;
+} device;
+typedef struct userinfo {
+  double balance;
+  bool powercut;
+  // Read
+  date last_read;
+  bool read_now;
+  int last_month_usage;
+  // basic
+  int No;
+  char name[32];
+  char id[19];
+  int type;
+  add address;
+  char password[32];
+  double voltage;
+} userinfo;
+typedef struct userbill {
+  // General
+  int caseNo;
+  userinfo user_record;
+  int rateNo;
+  // Usage
+  int last_month_usage;
+  int current_usage;
+  // Read status
+  bool read;
+  date read_date;
+  // Pay Status
+  int pay;
+  bool payment;
+  date payment_date;
+} userbill;
+typedef struct MRdef {
+  int No;
+  char name[32];
+  float progress;
+} MRdef;
+typedef struct rateRecord {
+  date createTime;
+  double ent[N][N];
+  double urban[N][N];
+  double rural[N][N];
+} rateRecord;
+typedef struct estateUserInfoNode {
+  userinfo info;
+  struct estateUserInfoNode* next;
+} estateUserInfoNode;
+// Functions ===========================================================
+rateRecord getCurrentRate() {
+  rateRecord current;
+  fstream file;
+  file.open(FLOC_RATE, ios::binary | ios::in);
+  float size = sizeof(rateRecord);
+  file.seekg(-size, ios::end);
+  file.read((char*)&current, sizeof(rateRecord));
+  file.close();
+  return current;
 }
-void getTotalUser()
-{
-	totalUser = 0;
-	fstream tmp;
-	tmp.open(FLOC_USERBASICINFO, ios::in | ios::binary);
-	tmp.seekg(0, ios::beg);
-	int begin;
-	begin = tmp.tellg();
-	tmp.seekg(0, ios::end);
-	int endFile;
-	endFile = tmp.tellg();
-	totalUser = (endFile - begin) / sizeof(userinfo);
+void getTotalUser() {
+  totalUser = 0;
+  fstream tmp;
+  tmp.open(FLOC_USERBASICINFO, ios::in | ios::binary);
+  tmp.seekg(0, ios::beg);
+  int begin;
+  begin = tmp.tellg();
+  tmp.seekg(0, ios::end);
+  int endFile;
+  endFile = tmp.tellg();
+  totalUser = (endFile - begin) / sizeof(userinfo);
 }
-userinfo getUserInfo(int username)
-{
-	userinfo use;
-	fstream temp;
-	temp.open(FLOC_USERBASICINFO, ios::binary | ios::in);
-	while (temp.read((char*)&use, sizeof(userinfo)))
-	{
-		if (use.No == username)
-		{
-			temp.close();
-			break;
-		}
-	}
-	return use;
+userinfo getUserInfo(int username) {
+  userinfo use;
+  fstream temp;
+  temp.open(FLOC_USERBASICINFO, ios::binary | ios::in);
+  while (temp.read((char*)&use, sizeof(userinfo))) {
+    if (use.No == username) {
+      temp.close();
+      break;
+    }
+  }
+  return use;
 }
-int genUserNo()
-{
-	fstream tmpf;
-	userinfo tmp;
-	tmpf.open(FLOC_USERBASICINFO, ios::in | ios::app | ios::binary);
-	int size = sizeof(userinfo);
-	tmpf.seekg(-size, ios::end);
-	tmpf.read((char*)&tmp, sizeof(userinfo));
-	return (tmp.No + 1);
+int genUserNo() {
+  fstream tmpf;
+  userinfo tmp;
+  tmpf.open(FLOC_USERBASICINFO, ios::in | ios::app | ios::binary);
+  int size = sizeof(userinfo);
+  tmpf.seekg(-size, ios::end);
+  tmpf.read((char*)&tmp, sizeof(userinfo));
+  return (tmp.No + 1);
 }
-void changeUserInfo(userinfo use)
-{
-	userinfo tempUserInfo;
-	fstream origin;
-	fstream temp;
-	origin.open(FLOC_USERBASICINFO, ios::binary | ios::in);
-	temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::out);
-	while (origin.read((char*)&tempUserInfo, sizeof(userinfo)))
-	{
-		if (tempUserInfo.No == use.No)
-			temp.write((char*)&use, sizeof(userinfo));
-		else
-			temp.write((char*)&tempUserInfo, sizeof(userinfo));
-	}
-	temp.close();
-	origin.close();
-	origin.open(FLOC_USERBASICINFO, ios::binary | ios::out);
-	temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::in);
-	while (temp.read((char*)&tempUserInfo, sizeof(userinfo)))
-	{
-		origin.write((char*)&tempUserInfo, sizeof(userinfo));
-	}
-	temp.close();
-	origin.close();
+void changeUserInfo(userinfo use) {
+  userinfo tempUserInfo;
+  fstream origin;
+  fstream temp;
+  origin.open(FLOC_USERBASICINFO, ios::binary | ios::in);
+  temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::out);
+  while (origin.read((char*)&tempUserInfo, sizeof(userinfo))) {
+    if (tempUserInfo.No == use.No)
+      temp.write((char*)&use, sizeof(userinfo));
+    else
+      temp.write((char*)&tempUserInfo, sizeof(userinfo));
+  }
+  temp.close();
+  origin.close();
+  origin.open(FLOC_USERBASICINFO, ios::binary | ios::out);
+  temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::in);
+  while (temp.read((char*)&tempUserInfo, sizeof(userinfo))) {
+    origin.write((char*)&tempUserInfo, sizeof(userinfo));
+  }
+  temp.close();
+  origin.close();
 }
--- Linux-Build/user.h
+++ Linux-Build/user.h
@@ -3,310 +3,305 @@
 #include "system.h"
 using namespace std;
 
-
-void changePasswordTo(int username, char* passput)
-{
-	userinfo user;
-	user = getUserInfo(username);
-	strcpy(user.password, passput);
-	changeUserInfo(user);
+void changePasswordTo(int username, char* passput) {
+  userinfo user;
+  user = getUserInfo(username);
+  strcpy(user.password, passput);
+  changeUserInfo(user);
 }
-bool findUser(int username)
-{
-	userinfo use;
-	fstream temp;
-	temp.open(FLOC_USERBASICINFO, ios::binary | ios::in);
-	while (!temp.eof())
-	{
-		temp.read((char*)&use, sizeof(userinfo));
-		if (use.No == username)
-		{
-			return true;
-		}
-	}
-	return false;
+bool findUser(int username) {
+  userinfo use;
+  fstream temp;
+  temp.open(FLOC_USERBASICINFO, ios::binary | ios::in);
+  while (!temp.eof()) {
+    temp.read((char*)&use, sizeof(userinfo));
+    if (use.No == username) {
+      return true;
+    }
+  }
+  return false;
 }
-bool validUser(int username)
-{
-	userinfo use;
-	use = getUserInfo(username);
-	if (use.No == username)
-		return true;
-	return false;
+bool validUser(int username) {
+  userinfo use;
+  use = getUserInfo(username);
+  if (use.No == username) return true;
+  return false;
 }
-string rtnType(int typenum)
-{
-	string typ;
-	switch (typenum)
-	{
-	case 1: typ = "Administrator"; break;
-	case 2: typ = "Meter Reader";  break;
-	case 3: typ = "Charger"; break;
-	case 4: typ = "Enterprise User (E1)"; break;
-	case 5: typ = "Enterprise User (E2)"; break;
-	case 6: typ = "Urban User"; break;
-	case 7: typ = "Common Rural User"; break;
-	case 8: typ = "Rural User in Poverty"; break;
-	}
-	return typ;
+string rtnType(int typenum) {
+  string typ;
+  switch (typenum) {
+    case 1:
+      typ = "Administrator";
+      break;
+    case 2:
+      typ = "Meter Reader";
+      break;
+    case 3:
+      typ = "Charger";
+      break;
+    case 4:
+      typ = "Enterprise User (E1)";
+      break;
+    case 5:
+      typ = "Enterprise User (E2)";
+      break;
+    case 6:
+      typ = "Urban User";
+      break;
+    case 7:
+      typ = "Common Rural User";
+      break;
+    case 8:
+      typ = "Rural User in Poverty";
+      break;
+  }
+  return typ;
 }
-int getUserType(int username)
-{
-	userinfo user;
-	user = getUserInfo(username);
-	return user.type;
+int getUserType(int username) {
+  userinfo user;
+  user = getUserInfo(username);
+  return user.type;
 }
-MRdef getMRDetail(int username)
-{
-	MRdef mr;
-	fstream temp;
-	temp.open(FLOC_MRINFO, ios::binary | ios::in);
-	while (temp.read((char*)&mr, sizeof(userinfo)))
-	{
-		if (mr.No == username)
-		{
-			temp.close();
-			break;
-		}
-	}
-	return mr;
+MRdef getMRDetail(int username) {
+  MRdef mr;
+  fstream temp;
+  temp.open(FLOC_MRINFO, ios::binary | ios::in);
+  while (temp.read((char*)&mr, sizeof(userinfo))) {
+    if (mr.No == username) {
+      temp.close();
+      break;
+    }
+  }
+  return mr;
 }
 // Meter Reader-specific ====================================================
 void showRate(rateRecord current = getCurrentRate()) {
-	cout << "Urban Rate Overview" << endl;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cout << setw(9) << current.urban[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << "------------------------------------" << endl;
-	cout << "Rural Rate Overview" << endl;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cout << setw(9) << current.rural[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << "-------------------------------------" << endl;
-	cout << "Enterprise Rate Overview" << endl;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cout << setw(9) << current.ent[i][j] << " ";
-		}
-		cout << endl;
-	}
+  cout << "Urban Rate Overview" << endl;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << setw(9) << current.urban[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << "------------------------------------" << endl;
+  cout << "Rural Rate Overview" << endl;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << setw(9) << current.rural[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << "-------------------------------------" << endl;
+  cout << "Enterprise Rate Overview" << endl;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << setw(9) << current.ent[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
 void storeMRInfo(MRdef mr) {
-	MRdef mrtmp;
-	fstream temp;
-	temp.open(FLOC_MRINFO, ios::binary | ios::app | ios::out);
-	temp.write((char*)&mr, sizeof(MRdef));
-	temp.close();
+  MRdef mrtmp;
+  fstream temp;
+  temp.open(FLOC_MRINFO, ios::binary | ios::app | ios::out);
+  temp.write((char*)&mr, sizeof(MRdef));
+  temp.close();
 }
 int getRateEdition() {
-	fstream file;
-	file.open(FLOC_RATE, ios::binary | ios::in);
-	file.seekg(0, ios::beg);
-	double head = file.tellg();
-	file.seekg(-(sizeof(rateRecord), ios::end));
-	double end = file.tellg();
-	return end - head;
+  fstream file;
+  file.open(FLOC_RATE, ios::binary | ios::in);
+  file.seekg(0, ios::beg);
+  double head = file.tellg();
+  file.seekg(-(sizeof(rateRecord), ios::end));
+  double end = file.tellg();
+  return end - head;
 }
 int genRateNo(int type, int row, int col) {
-	date currentTime = getCurrentTime();
-	int rateNo = 0;
-	rateNo = getRateEdition();
-	rateNo = rateNo * 10 + type;
-	rateNo = rateNo * 10 + row;
-	rateNo = rateNo * 10 + col;
-	return rateNo;
+  date currentTime = getCurrentTime();
+  int rateNo = 0;
+  rateNo = getRateEdition();
+  rateNo = rateNo * 10 + type;
+  rateNo = rateNo * 10 + row;
+  rateNo = rateNo * 10 + col;
+  return rateNo;
 }
-void chargeFeedback (int username, int lastUsage, int currentUsage, int& rateNo, double& fee) {
-	rateRecord rate = getCurrentRate();
-	userinfo user = getUserInfo(username);
-	int row = 0;
-	int col = 0;
-	user.voltage = user.voltage / 1000;
-	cout << "voltage: " << user.voltage << endl;
-	cout << "Current Use: " << currentUsage << endl;
-	cout << "Used : " << currentUsage - lastUsage << endl;
-	if (user.type == 6) {
-		for (row = 0; rate.urban[row][0] != -1; row++) {
-			if (currentUsage >= rate.urban[row][0] && currentUsage < rate.urban[row + 1][0]) {
-				break;
-			}
-			else if (currentUsage > rate.urban[row][0] && rate.urban[row + 1][0] == -1) {
-				break;
-			}
-		}
-		for (col = 0; rate.urban[0][col] != -1; col++) {
-			if (user.voltage >= rate.urban[0][col] && user.voltage < rate.urban[0][col + 1]) {
-				col++;
-				break;
-			}
-			else if (rate.urban[0][col + 1] == -1 && user.voltage > rate.urban[0][col]) {
-				break;
-			}
-		}
-		fee = (currentUsage - lastUsage) * rate.urban[row][col];
-	}
-	else if (user.type == 7) {
-		for (col = 0; rate.rural[0][col] != -1; col++) {
-			if (user.voltage >= rate.rural[0][col] && currentUsage < rate.rural[0][col + 1]) {
-				col++;
-				break;
-			}
-			else if (rate.rural[0][col + 1] == -1 && currentUsage >= rate.rural[0][col]) {
-				break;
-			}
-		}
-		fee = (currentUsage - lastUsage) * rate.rural[1][col];
-	}
-	else if (user.type == 4) {
-		for (col = 0; rate.ent[0][col] != -1; col++) {
-			if (user.voltage >= rate.ent[0][col] && user.voltage < rate.ent[0][col + 1]) {
-				col++;
-				break;
-			}
-			else if (rate.ent[0][col + 1] == -1 && user.voltage >= rate.ent[0][col]) {
-				break;
-			}
-		}
-		fee = (currentUsage - lastUsage) * rate.ent[1][col];
-	}
-	else if (user.type == 5) {
-		for (col = 0; rate.ent[0][col] != -1; col++) {
-			if (currentUsage >= rate.ent[0][col] && currentUsage < rate.ent[1][col + 1]) {
-				col++;
-				break;
-			}
-			else if (rate.ent[0][col + 1] == -1 && currentUsage >=rate.ent[0][col]) {
-				break;
-			}
-			fee = (currentUsage - lastUsage) * rate.ent[2][col];
-		}
-	}
-	cout << endl;
-	cout << "row: " << row << " col: " << col << endl;
-	cout << "rate: "<< rate.urban[row][col] << endl;
-	cout << "fee: " << fee << endl;
+void chargeFeedback(int username, int lastUsage, int currentUsage, int& rateNo, double& fee) {
+  rateRecord rate = getCurrentRate();
+  userinfo user = getUserInfo(username);
+  int row = 0;
+  int col = 0;
+  user.voltage = user.voltage / 1000;
+  cout << "voltage: " << user.voltage << endl;
+  cout << "Current Use: " << currentUsage << endl;
+  cout << "Used : " << currentUsage - lastUsage << endl;
+  if (user.type == 6) {
+    for (row = 0; rate.urban[row][0] != -1; row++) {
+      if (currentUsage >= rate.urban[row][0] &&
+          currentUsage < rate.urban[row + 1][0]) {
+        break;
+      } else if (currentUsage > rate.urban[row][0] &&
+                 rate.urban[row + 1][0] == -1) {
+        break;
+      }
+    }
+    for (col = 0; rate.urban[0][col] != -1; col++) {
+      if (user.voltage >= rate.urban[0][col] &&
+          user.voltage < rate.urban[0][col + 1]) {
+        col++;
+        break;
+      } else if (rate.urban[0][col + 1] == -1 &&
+                 user.voltage > rate.urban[0][col]) {
+        break;
+      }
+    }
+    fee = (currentUsage - lastUsage) * rate.urban[row][col];
+  } else if (user.type == 7) {
+    for (col = 0; rate.rural[0][col] != -1; col++) {
+      if (user.voltage >= rate.rural[0][col] &&
+          currentUsage < rate.rural[0][col + 1]) {
+        col++;
+        break;
+      } else if (rate.rural[0][col + 1] == -1 &&
+                 currentUsage >= rate.rural[0][col]) {
+        break;
+      }
+    }
+    fee = (currentUsage - lastUsage) * rate.rural[1][col];
+  } else if (user.type == 4) {
+    for (col = 0; rate.ent[0][col] != -1; col++) {
+      if (user.voltage >= rate.ent[0][col] &&
+          user.voltage < rate.ent[0][col + 1]) {
+        col++;
+        break;
+      } else if (rate.ent[0][col + 1] == -1 &&
+                 user.voltage >= rate.ent[0][col]) {
+        break;
+      }
+    }
+    fee = (currentUsage - lastUsage) * rate.ent[1][col];
+  } else if (user.type == 5) {
+    for (col = 0; rate.ent[0][col] != -1; col++) {
+      if (currentUsage >= rate.ent[0][col] &&
+          currentUsage < rate.ent[1][col + 1]) {
+        col++;
+        break;
+      } else if (rate.ent[0][col + 1] == -1 &&
+                 currentUsage >= rate.ent[0][col]) {
+        break;
+      }
+      fee = (currentUsage - lastUsage) * rate.ent[2][col];
+    }
+  }
+  cout << endl;
+  cout << "row: " << row << " col: " << col << endl;
+  cout << "rate: " << rate.urban[row][col] << endl;
+  cout << "fee: " << fee << endl;
 }
 void updateUserReadStatus(int username, int currentUsage, int fee) {
-	fstream file;
-	file.open(FLOC_USERBASICINFO, ios::binary | ios::in | ios::out | ios::app);
-	while(1) {
-		userinfo currentUser;
-		file.read((char*)&currentUser, sizeof(userinfo));
-		if (file.eof()) {
-			file.close();
-			cout << "Cannot find the user" << endl;
-			return;
-		}
-		if (currentUser.No == username) {
-			currentUser.last_month_usage = currentUsage;
-			currentUser.balance = currentUser.balance - fee;
-			file.seekg(-(sizeof(userinfo), ios::cur));
-			file.write((char*)&currentUser, sizeof(userinfo));
-			file.close();
-		}
-	}
+  fstream file;
+  file.open(FLOC_USERBASICINFO, ios::binary | ios::in | ios::out | ios::app);
+  while (1) {
+    userinfo currentUser;
+    file.read((char*)&currentUser, sizeof(userinfo));
+    if (file.eof()) {
+      file.close();
+      cout << "Cannot find the user" << endl;
+      return;
+    }
+    if (currentUser.No == username) {
+      currentUser.last_month_usage = currentUsage;
+      currentUser.balance = currentUser.balance - fee;
+      file.seekg(-(sizeof(userinfo), ios::cur));
+      file.write((char*)&currentUser, sizeof(userinfo));
+      file.close();
+    }
+  }
 }
 void pushReadToHistory(int username, int lastUsage, int currentUsage, int rateNo, double fee) {
-	userinfo user = getUserInfo(username);
-	userbill bill;
-	bill.caseNo = genCaseNo();
-	bill.user_record = user;
-	bill.last_month_usage = user.last_month_usage;
-	bill.read_date = getCurrentTime();
-	bill.read = true;
-	chargeFeedback(username, lastUsage, currentUsage, bill.rateNo, fee);
-	fstream file;
-	file.open(FLOC_BILLDETAIL, ios::binary | ios::out | ios::app);
-	file.write((char*)&bill, sizeof(userbill));
-	file.close();
+  userinfo user = getUserInfo(username);
+  userbill bill;
+  bill.caseNo = genCaseNo();
+  bill.user_record = user;
+  bill.last_month_usage = user.last_month_usage;
+  bill.read_date = getCurrentTime();
+  bill.read = true;
+  chargeFeedback(username, lastUsage, currentUsage, bill.rateNo, fee);
+  fstream file;
+  file.open(FLOC_BILLDETAIL, ios::binary | ios::out | ios::app);
+  file.write((char*)&bill, sizeof(userbill));
+  file.close();
 }
 // end of update=============================
-void changeMRInfo(MRdef mr)
-{
-	MRdef mrtmp;
-	fstream origin;
-	fstream temp;
-	origin.open(FLOC_MRINFO, ios::binary | ios::in);
-	temp.open(FLOC_EDITMRINFO_TEMP, ios::binary | ios::out);
-	while (origin.read((char*)&mrtmp, sizeof(MRdef)))
-	{
-		if (mrtmp.No == mr.No)
-			temp.write((char*)&mr, sizeof(MRdef));
-		else
-			temp.write((char*)&mrtmp, sizeof(MRdef));
-	}
-	temp.close();
-	origin.close();
-	origin.open(FLOC_USERBASICINFO, ios::binary | ios::out);
-	temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::in);
-	while (temp.read((char*)&mrtmp, sizeof(MRdef)))
-	{
-		origin.write((char*)&mrtmp, sizeof(MRdef));
-	}
-	temp.close();
-	origin.close();
+void changeMRInfo(MRdef mr) {
+  MRdef mrtmp;
+  fstream origin;
+  fstream temp;
+  origin.open(FLOC_MRINFO, ios::binary | ios::in);
+  temp.open(FLOC_EDITMRINFO_TEMP, ios::binary | ios::out);
+  while (origin.read((char*)&mrtmp, sizeof(MRdef))) {
+    if (mrtmp.No == mr.No)
+      temp.write((char*)&mr, sizeof(MRdef));
+    else
+      temp.write((char*)&mrtmp, sizeof(MRdef));
+  }
+  temp.close();
+  origin.close();
+  origin.open(FLOC_USERBASICINFO, ios::binary | ios::out);
+  temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::in);
+  while (temp.read((char*)&mrtmp, sizeof(MRdef))) {
+    origin.write((char*)&mrtmp, sizeof(MRdef));
+  }
+  temp.close();
+  origin.close();
 }
-void deleteMRInfo(MRdef mr)
-{
-	MRdef mrtmp;
-	fstream origin;
-	fstream temp;
-	origin.open(FLOC_MRINFO, ios::binary | ios::in);
-	temp.open(FLOC_EDITMRINFO_TEMP, ios::binary | ios::out);
-	while (origin.read((char*)&mrtmp, sizeof(MRdef)))
-	{
-		if (mrtmp.No == mr.No)
-			continue;
-		else
-			temp.write((char*)&mrtmp, sizeof(userinfo));
-	}
-	temp.close();
-	origin.close();
-	origin.open(FLOC_MRINFO, ios::binary | ios::out);
-	temp.open(FLOC_EDITMRINFO_TEMP, ios::binary | ios::in);
-	while (temp.read((char*)&mrtmp, sizeof(MRdef)))
-	{
-		origin.write((char*)&mrtmp, sizeof(MRdef));
-	}
-	temp.close();
-	origin.close();
+void deleteMRInfo(MRdef mr) {
+  MRdef mrtmp;
+  fstream origin;
+  fstream temp;
+  origin.open(FLOC_MRINFO, ios::binary | ios::in);
+  temp.open(FLOC_EDITMRINFO_TEMP, ios::binary | ios::out);
+  while (origin.read((char*)&mrtmp, sizeof(MRdef))) {
+    if (mrtmp.No == mr.No)
+      continue;
+    else
+      temp.write((char*)&mrtmp, sizeof(userinfo));
+  }
+  temp.close();
+  origin.close();
+  origin.open(FLOC_MRINFO, ios::binary | ios::out);
+  temp.open(FLOC_EDITMRINFO_TEMP, ios::binary | ios::in);
+  while (temp.read((char*)&mrtmp, sizeof(MRdef))) {
+    origin.write((char*)&mrtmp, sizeof(MRdef));
+  }
+  temp.close();
+  origin.close();
 }
 // Commom ===================================================================
-void storeUserInfo(userinfo use)
-{
-	fstream temp;
-	temp.open(FLOC_USERBASICINFO, ios::binary | ios::app | ios::out);
-	temp.write((char*)&use, sizeof(userinfo));
-	temp.close();
+void storeUserInfo(userinfo use) {
+  fstream temp;
+  temp.open(FLOC_USERBASICINFO, ios::binary | ios::app | ios::out);
+  temp.write((char*)&use, sizeof(userinfo));
+  temp.close();
 }
-void deleteUserInfo(userinfo use)
-{
-	userinfo tmpInfo;
-	fstream origin;
-	fstream temp;
-	origin.open(FLOC_USERBASICINFO, ios::binary | ios::in);
-	temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::out);
-	while (origin.read((char*)&tmpInfo, sizeof(userinfo)))
-	{
-		if (tmpInfo.No == use.No)
-			continue;
-		else
-			temp.write((char*)&tmpInfo, sizeof(userinfo));
-	}
-	temp.close();
-	origin.close();
-	origin.open(FLOC_USERBASICINFO, ios::binary | ios::out);
-	temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::in);
-	while (temp.read((char*)&tmpInfo, sizeof(userinfo)))
-	{
-		origin.write((char*)&tmpInfo, sizeof(userinfo));
-	}
-	temp.close();
-	origin.close();
+void deleteUserInfo(userinfo use) {
+  userinfo tmpInfo;
+  fstream origin;
+  fstream temp;
+  origin.open(FLOC_USERBASICINFO, ios::binary | ios::in);
+  temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::out);
+  while (origin.read((char*)&tmpInfo, sizeof(userinfo))) {
+    if (tmpInfo.No == use.No)
+      continue;
+    else
+      temp.write((char*)&tmpInfo, sizeof(userinfo));
+  }
+  temp.close();
+  origin.close();
+  origin.open(FLOC_USERBASICINFO, ios::binary | ios::out);
+  temp.open(FLOC_EDITUSERINFO_TEMP, ios::binary | ios::in);
+  while (temp.read((char*)&tmpInfo, sizeof(userinfo))) {
+    origin.write((char*)&tmpInfo, sizeof(userinfo));
+  }
+  temp.close();
+  origin.close();
 }
